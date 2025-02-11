4310537d26328ffb982a4be1158b48a584fbe1cd
n0body <n0bodyV2@proton.me>
2025-02-11 16:28:42 -0300

    commit inicial

---
+++ bitstamp.py
@@ -0,0 +1,45 @@
+import websocket
+import json
+
+def ao_abrir(ws):
+    print('Conexão estabelecida com sucesso!')
+
+    json_subscribe = """    
+{
+    "event": "bts:subscribe",
+    "data": {
+        "channel": "live_trades_btcusd"
+    }
+}
+"""
+
+    ws.send(json_subscribe)
+
+def ao_fechar(ws):
+    print('Conexão encerrada!')
+
+def erro(ws, erro):
+    print('---------------------------------')
+    print('Algo deu errado!')
+    print('---------------------------------')
+
+
+def ao_receber_mensagem(ws, mensagem):
+    mensagem = json.loads(mensagem)
+    price = mensagem["data"]["price"]
+    print(price)
+
+def comprar():
+    pass
+
+def vender():
+    pass
+
+if __name__ == "__main__":
+    ws = websocket.WebSocketApp("wss://ws.bitstamp.net/",
+                                on_open=ao_abrir,
+                                on_message=ao_receber_mensagem,
+                                on_error=erro,
+                                on_close=ao_fechar
+                                )
+ws.run_forever()
